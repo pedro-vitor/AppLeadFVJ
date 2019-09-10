51cb8e44cf69f1e64c82e3c98e74522826b42030
hitdary <hitdary@gmail.com>
2019-09-10 13:01:47 -0300

    Layout List and Layout Background Color Updated

--- app/src/main/java/com/NTI/AppFVJ/Activity/MainActivity.java
+++ app/src/main/java/com/NTI/AppFVJ/Activity/MainActivity.java
@@ -1,11 +1,21 @@
 package com.NTI.AppFVJ.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
-import com.NTI.AppFVJ.Adapters.LeadsAdapter;
+import com.NTI.AppFVJ.Adapter.LeadsAdapter;
 import com.NTI.AppFVJ.Models.Leads;
 import com.NTI.AppFVJ.R;
 
@@ -18,14 +28,93 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView LeadsListView = findViewById(R.id.LeadsList);
+        ListView List = findViewById(R.id.LeadsList);
 
-        List<Leads> LeadsList = new ArrayList<Leads>();
-/*
-        for (int Lead = 0; Lead < 10; Lead++)
-            LeadsList.add(new Leads());
-*/
-        LeadsAdapter Adapter = new LeadsAdapter(this, LeadsList);
-        LeadsListView.setAdapter(Adapter);
+        List<Leads> ListLeads = new ArrayList<Leads>();
+
+        Leads Le = new Leads();
+        Le.setTown("Aracati");
+        Le.setName("Arthur da Silva Braga");
+
+        Leads Le1 = new Leads();
+        Le1.setTown("Fortim");
+        Le1.setName("Davi Lima Silva");
+
+        Leads Le2 = new Leads();
+        Le2.setTown("Icapui");
+        Le2.setName("Izabelle Silva Costa");
+
+        Leads Le3 = new Leads();
+        Le3.setTown("Russas");
+        Le3.setName("Maria Correia Silva");
+
+        Leads Le4 = new Leads();
+        Le4.setTown("Itaiçaba");
+        Le4.setName("Matheus Oliveira dos Santos");
+
+        Leads Le5 = new Leads();
+        Le5.setTown("Itaiçaba");
+        Le5.setName("Felipe Barros Lima");
+
+        Leads Le6 = new Leads();
+        Le6.setTown("Pontal");
+        Le6.setName("Rafaela Barbosa");
+
+        Leads Le7 = new Leads();
+        Le7.setTown("Santa Tereza");
+        Le7.setName("Armando Teobaldo Silva");
+
+        Leads Le8 = new Leads();
+        Le8.setTown("Aracati");
+        Le8.setName("Rosangela Torres Lima");
+
+        ListLeads.add(Le);
+        ListLeads.add(Le1);
+        ListLeads.add(Le2);
+        ListLeads.add(Le3);
+        ListLeads.add(Le4);
+        ListLeads.add(Le5);
+        ListLeads.add(Le6);
+        ListLeads.add(Le7);
+        ListLeads.add(Le8);
+
+
+        List.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            /*
+                RelativeLayout LayoutMain = (RelativeLayout)findViewById(R.id.activity_main);
+                View LayoutOptions = LayoutInflater.from(getApplicationContext()).inflate(R.layout.options_leads_list, null);
+                LayoutMain.addView(LayoutOptions);
+             */
+            }
+        });
+
+        LeadsAdapter Adapter = new LeadsAdapter(this, ListLeads);
+        List.setAdapter(Adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Profile:
+                break;
+            case R.id.Register:
+                break;
+            case R.id.Logout:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
     }
 }
--- app/src/main/java/com/NTI/AppFVJ/Adapter/LeadsAdapter.java
+++ app/src/main/java/com/NTI/AppFVJ/Adapter/LeadsAdapter.java
@@ -1,7 +1,4 @@
-package com.NTI.AppFVJ.Adapters;
-
-import com.NTI.AppFVJ.Models.Leads;
-import com.NTI.AppFVJ.R;
+package com.NTI.AppFVJ.Adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -10,11 +7,14 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.NTI.AppFVJ.Models.Leads;
+import com.NTI.AppFVJ.R;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 
 public class LeadsAdapter extends ArrayAdapter<Leads> {
     public LeadsAdapter(@NonNull Context context, @NonNull List<Leads> objects) {
@@ -24,21 +24,23 @@ public class LeadsAdapter extends ArrayAdapter<Leads> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View RowView = convertView;
 
-        if (RowView == null)
-            RowView = LayoutInflater.from(getContext()).inflate(R.layout.adapter_leads_list, parent, false);
+        View rowView = convertView;
 
-        Leads Leads = getItem(position);
+        if (rowView == null)
+            rowView = LayoutInflater.from(getContext()).inflate(R.layout.adapter_leads_list, parent, false);
 
         TextView Name, Town;
 
-        Name = RowView.findViewById(R.id.Name);
-        Town = RowView.findViewById(R.id.Town);
+        Name = rowView.findViewById(R.id.Name);
+        Town = rowView.findViewById(R.id.Town);
+
+        Leads Leads = getItem(position);
 
         Name.setText(Leads.getName());
         Town.setText(Leads.getTown());
 
-        return RowView;
+        return rowView;
     }
+
 }
