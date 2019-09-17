87bad235106d3c129be8e8cd5f8faf157de86dce
Pedro Vitor <pedrodeamons12@gmail.com>
2019-09-17 13:18:14 -0300

    adicionando a tela de profile

---
+++ app/src/main/java/com/NTI/AppFVJ/Activity/UserProfileActivity.java
@@ -0,0 +1,16 @@
+package com.NTI.AppFVJ.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.NTI.AppFVJ.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
